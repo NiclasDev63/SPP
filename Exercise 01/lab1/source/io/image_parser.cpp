ab6a7fec6034a3aecd41ea72318bc50174c4e4cc
Lukas Horn <hornlu.lh@gmail.com>
2023-11-06 13:28:01 +0100

    test

--- Exercise 01/lab1/source/io/image_parser.cpp
+++ Exercise 01/lab1/source/io/image_parser.cpp
@@ -4,13 +4,16 @@
 #include "image/bitmap_image.h"
 #include "pixel.h"
 
-BitmapImage ImageParser::read_bitmap(std::filesystem::path path)
+using namespace std;
+
+//testt
+BitmapImage ImageParser::read_bitmap(filesystem::path path)
 {
-    if (!std::filesystem::exists(path))
+    if (!filesystem::exists(path))
     {
-        throw std::exception();
+        throw exception();
     }
-    std::ifstream file(path, std::ios::binary);
+    ifstream file(path, ios::binary);
     if (file.is_open())
     {
         char header[54];
@@ -22,7 +25,7 @@ BitmapImage ImageParser::read_bitmap(std::filesystem::path path)
 
 
         if (width <= 0 || height <= 0 || bfOffBits <= 0) {
-            throw std::runtime_error("Ungültige Datei");
+            throw runtime_error("Ungültige Datei");
         }
 
         BitmapImage image = BitmapImage(height, width);
