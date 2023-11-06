a9fc9daa41d2afb81e15aa7c8589b27d3ebfbbe4
Lukas Horn <hornlu.lh@gmail.com>
2023-11-06 12:38:03 +0100

    corrected empty method body

--- Exercise 01/lab1/source/image/pixel.h
+++ Exercise 01/lab1/source/image/pixel.h
@@ -19,9 +19,9 @@ public:
 	static ChannelOrder channel_order;
 
 	// standard constructor
-    Pixel() : red(0), green(0), blue(0) { }
+    Pixel() : red(0), green(0), blue(0);
 
-	Pixel(T red, T green, T blue) : red(red), green(green), blue(blue){}
+	Pixel(T red, T green, T blue) : red(red), green(green), blue(blue);
 
 	// returns the intensitiy of the respective color
 	[[nodiscard]] T get_red_channel() const noexcept;
@@ -29,7 +29,7 @@ public:
 	[[nodiscard]] T get_blue_channel() const noexcept;
 
 	// overload the "==" operator
-	bool operator==(const Pixel &other) const {}
+	bool operator==(const Pixel &other) const;
 
 private:
 	T red;
