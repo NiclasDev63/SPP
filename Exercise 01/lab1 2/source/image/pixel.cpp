3e04246a91a1d828696033d648272fb061a05aab
NiclasDev63 <niclas.gregor20@gmail.com>
2023-11-06 17:42:49 +0100

    removed second lab folder

--- Exercise 01/lab1 2/source/image/pixel.cpp
+++
@@ -1,1 +0,0 @@
-#include "image/pixel.h"
--- Exercise 01/lab1 2/test/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "test.h"
-
-int main(int argc, char** argv) {
-	::testing::InitGoogleTest(&argc, argv);
-
-	const auto tests_return_code = RUN_ALL_TESTS();
-
-	return tests_return_code;
-}
