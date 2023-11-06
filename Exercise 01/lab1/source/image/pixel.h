b373adf65079fd6bb24b0d7139299d273412d1c9
Lukas Horn <hornlu.lh@gmail.com>
2023-11-06 12:34:59 +0100

    corrected empty method body

--- Exercise 01/lab1/source/image/pixel.h
+++ Exercise 01/lab1/source/image/pixel.h
@@ -24,9 +24,9 @@ public:
 	Pixel(T red, T green, T blue) : red(red), green(green), blue(blue){}
 
 	// returns the intensitiy of the respective color
-	[[nodiscard]] T get_red_channel() const noexcept {}
-	[[nodiscard]] T get_green_channel() const noexcept {}
-	[[nodiscard]] T get_blue_channel() const noexcept {}
+	[[nodiscard]] T get_red_channel() const noexcept;
+	[[nodiscard]] T get_green_channel() const noexcept;
+	[[nodiscard]] T get_blue_channel() const noexcept;
 
 	// overload the "==" operator
 	bool operator==(const Pixel &other) const {}
