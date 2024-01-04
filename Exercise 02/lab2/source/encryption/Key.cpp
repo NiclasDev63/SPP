507938ac29206d98560b395e01b9c3a2afc3c0be
NiclasDev63 <niclas.gregor20@gmail.com>
2024-01-04 18:52:19 +0100

    moved critical construct to correct spot in find_key_parallel

--- Exercise 02/lab2/source/encryption/Key.cpp
+++ Exercise 02/lab2/source/encryption/Key.cpp
@@ -99,11 +99,11 @@ Key::key_type Key::find_key_parallel(std::span<const key_type> keys, std::uint64
     #pragma omp parallel for num_threads(num_threads)
         for (auto i = std::size_t(0); i < keys.size(); i++) {
             auto hash = Key::hash(keys[i]);
-                if(hash == reference_hash)
-                {
+            if(hash == reference_hash)
+            {
                 #pragma omp critical
-                    result_key = keys[i];
-                }
+                result_key = keys[i];
+            }
     }
 
     return result_key;
