e12932b0b04fc42977a39a4a6015e351aee88a86
NiclasDev63 <niclas.gregor20@gmail.com>
2024-01-05 13:54:48 +0100

    merged changes from lichtenberg

--- Exercise 02/lab2/source/encryption/Key.cpp
+++ Exercise 02/lab2/source/encryption/Key.cpp
@@ -2,6 +2,9 @@
 #include "util/Hash.h"
 #include <limits>
 #include <iostream>
+#include <cstring>
+
+using namespace std;
 
 Key::key_type Key::get_standard_key() noexcept {
 	auto key = key_type{};
@@ -15,28 +18,28 @@ Key::key_type Key::get_standard_key() noexcept {
 Key::key_type Key::produce_new_key(Key::key_type key) noexcept {
     auto new_key = key_type{};
     for (auto i = key_type::value_type(0); i < 6; i++) {
-        std::uint64_t tmpval;
-        std::memcpy(&tmpval, &key[i * 8], sizeof(tmpval));
+        uint64_t tmpval;
+        memcpy(&tmpval, &key[i * 8], sizeof(tmpval));
         tmpval = Hash::hash(tmpval);
-        std::memcpy(&new_key[i * 8], &tmpval, sizeof(tmpval));
+        memcpy(&new_key[i * 8], &tmpval, sizeof(tmpval));
     }
 
     return new_key;
 }
 
-std::uint64_t Key::hash(Key::key_type key) noexcept {
+uint64_t Key::hash(Key::key_type key) noexcept {
 	std::uint64_t hash = 0;
 	for (auto i = key_type::value_type(0); i < 6; i++) {
-		std::uint64_t tmpval;
-		std::memcpy(&tmpval, &key[i * 8], sizeof(tmpval));
-		std::uint64_t tmphash = Hash::hash(tmpval);
+		uint64_t tmpval;
+		memcpy(&tmpval, &key[i * 8], sizeof(tmpval));
+		uint64_t tmphash = Hash::hash(tmpval);
 		hash = Hash::combine_hashes(hash, tmphash);
 	}
 
 	return hash;
 }
 
-std::uint64_t Key::get_smallest_hash(std::span<const key_type> keys) noexcept {
+uint64_t Key::get_smallest_hash(std::span<const key_type> keys) noexcept {
 	Hash::hash_type smallest_hash = std::numeric_limits<Hash::hash_type>::max();
     for(const auto & key : keys) {
         auto hash = Key::hash(key);
@@ -46,7 +49,7 @@ std::uint64_t Key::get_smallest_hash(std::span<const key_type> keys) noexcept {
 	return smallest_hash;
 }
 
-std::uint64_t Key::get_smallest_hash_parallel(std::span<const key_type> keys,  const int num_threads) noexcept {
+uint64_t Key::get_smallest_hash_parallel(span<const key_type> keys,  const int num_threads) noexcept {
 	if(keys.empty()) {
 		return std::numeric_limits<std::uint64_t>::max();
 	}
@@ -61,7 +64,7 @@ std::uint64_t Key::get_smallest_hash_parallel(std::span<const key_type> keys,  c
 	return smallest_hash;
 }
 
-Key::key_type Key::find_key(std::span<const key_type> keys, std::uint64_t reference_hash) noexcept {
+Key::key_type Key::find_key(span<const key_type> keys, uint64_t reference_hash) noexcept {
     auto result_key = key_type {};
     for (auto i = std::size_t(0); i < keys.size(); i++) {
         auto hash = Key::hash(keys[i]);
@@ -72,7 +75,7 @@ Key::key_type Key::find_key(std::span<const key_type> keys, std::uint64_t refere
     return result_key;
 }
 
-Key::key_type Key::find_key_parallel(std::span<const key_type> keys, std::uint64_t reference_hash, const int num_threads) noexcept {
+Key::key_type Key::find_key_parallel(std::span<const key_type> keys, uint64_t reference_hash, const int num_threads) noexcept {
     auto result_key = key_type {};
 
     #pragma omp parallel for num_threads(num_threads)
