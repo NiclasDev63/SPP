18ab079e389cb8d230931f266830bc440e24c527
NiclasDev63 <niclas.gregor20@gmail.com>
2024-01-25 14:05:39 +0100

    finished 2

--- Exercise03/lab3/lab3/source/cuda/image.cu
+++ Exercise03/lab3/lab3/source/cuda/image.cu
@@ -5,7 +5,7 @@
 #include <cuda_runtime_api.h>
 #include <device_launch_parameters.h>
 
-//TODO kompiliert nocht nicht vollständig
+//TODO kompiliert nocht nicht vollständig Error:"calling a host function("Pixel<unsigned char> ::get_red_channel() const") from a global function("grayscale_kernel") is not allowed"
 __global__ void grayscale_kernel(const Pixel<std::uint8_t>* const input, Pixel<std::uint8_t>* const output, const unsigned int width, const unsigned int height) {
     //Calculate the global threadID
     unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
@@ -18,6 +18,7 @@ __global__ void grayscale_kernel(const Pixel<std::uint8_t>* const input, Pixel<s
         const auto g = pixel.get_green_channel();
         const auto b = pixel.get_blue_channel();
 
+
         // Calculate the grayscale value
         const auto gray = r * 0.2989 + g * 0.5870 + b * 0.1140;
         const auto gray_converted = static_cast<std::uint8_t>(gray);
@@ -26,4 +27,49 @@ __global__ void grayscale_kernel(const Pixel<std::uint8_t>* const input, Pixel<s
 
         output[index] = gray_pixel;
     }
+}
+
+BitmapImage get_grayscale_cuda(const BitmapImage& source) {
+    const size_t num_pixels = source.get_width() * source.get_height();
+    Pixel<std::uint8_t>* output;
+    Pixel<std::uint8_t>* device_input;
+    Pixel<std::uint8_t>* device_output;
+
+    // Allocate memory on the device
+    cudaMalloc((void**)&device_input, num_pixels * sizeof(Pixel<std::uint8_t>));
+    cudaMalloc((void**)&device_output, num_pixels * sizeof(Pixel<std::uint8_t>));
+
+    // Copy input data to the device
+    cudaMemcpy(device_input, source.get_data(), num_pixels * sizeof(Pixel<std::uint8_t>), cudaMemcpyHostToDevice);
+
+    dim3 blockDim(16, 16);
+    dim3 gridDim(divup(source.get_width(), blockDim.x), divup(source.get_height(), blockDim.y));
+
+    // Call the kernel
+    grayscale_kernel<<<gridDim, blockDim>>>(device_input, device_output, source.get_width(), source.get_height());
+
+    // Wait for the kernel to finish
+    cudaDeviceSynchronize();
+
+    // Copy the result back to the host
+    output = new Pixel<std::uint8_t>[num_pixels];
+    cudaMemcpy(output, device_output, num_pixels * sizeof(Pixel<std::uint8_t>), cudaMemcpyDeviceToHost);
+
+    BitmapImage result{source.get_width(), source.get_height()};
+    
+    for (unsigned int x = 0; x < source.get_width(); x++) {
+        for (unsigned int y = 0; y < source.get_height(); y++) {
+            const auto pixel = output[y * source.get_width() + x];
+            result.set_pixel(x, y, pixel);
+        }
+    }
+
+    // Free the device memory
+    cudaFree(device_input);
+    cudaFree(device_output);
+
+    // Free the host memory
+    delete[] output;
+
+    return result;
 }
---
+++ Exercise03/lab3/lab3/source/encryption/Algorithm.h
@@ -0,0 +1,80 @@
+#pragma once
+#include <array>
+#include <cstdint>
+#include <exception>
+#include "Algorithm.h"
+#include "image/bitmap_image.h"
+#include "Key.h"
+#include "FES.h"
+
+enum EncryptionStep {
+    E,
+    D,
+    K,
+    T
+};
+
+using EncryptionScheme = std::array<EncryptionStep, 16>;
+
+std::uint64_t encode(const EncryptionScheme& scheme) {
+    std::uint64_t result = 0;
+
+    for (std::size_t i = 0; i < scheme.size(); ++i) {
+        switch (scheme[i]) {
+            case E: break; // 00
+            case D: result |= (1ULL << (2 * i)); break; // 01
+            case K: result |= (2ULL << (2 * i)); break; // 10
+            case T: result |= (3ULL << (2 * i)); break; // 11
+        }
+    }
+
+    result |= (result << 32);
+
+    return result;
+}
+
+
+EncryptionScheme decode(std::uint64_t code) {
+    //check if first 32 bits are equal to last 32 bits
+    if ((code & 0xFFFFFFFF) != ((code >> 32) & 0xFFFFFFFF)) {
+        throw std::exception();
+    }
+
+    EncryptionScheme scheme;
+
+    for (std::size_t i = 0; i < scheme.size(); ++i) {
+        //get first 2 bits from code
+        std::uint64_t bits = (code >> (2 * i)) & 3;
+
+        switch (bits) {
+            case 0: scheme[i] = E; break; // 00
+            case 1: scheme[i] = D; break; // 01
+            case 2: scheme[i] = K; break; // 10
+            case 3: scheme[i] = T; break; // 11
+            default: throw std::exception();
+        }
+    }
+
+    return scheme;
+}
+
+
+BitmapImage perform_scheme(BitmapImage image, Key::key_type key, const EncryptionScheme& scheme) {
+    for (std::size_t i = 0; i < scheme.size(); ++i) {
+        switch (scheme[i]) {
+            case E: 
+                image = FES::encrypt(image, key); 
+                break;
+            case D: 
+                image = FES::decrypt(image, key); 
+                break;
+            case T: 
+                image.transpose(); 
+                break;
+            case K: 
+                key = Key::produce_new_key(key); 
+                break;
+        }
+    }
+    return image;
+}
